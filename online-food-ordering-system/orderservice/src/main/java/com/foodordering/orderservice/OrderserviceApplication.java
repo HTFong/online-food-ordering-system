8bcb929003c1cf0f2834eea9765e985d2ef3e334
phong <darkmirror2000@gmail.com>
2025-09-07 19:27:10 +0700

    link orderserver - configserver

--- online-food-ordering-system/orderservice/src/main/java/com/foodordering/orderservice/OrderserviceApplication.java
+++ online-food-ordering-system/orderservice/src/main/java/com/foodordering/orderservice/OrderserviceApplication.java
@@ -2,12 +2,15 @@ package com.foodordering.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class OrderserviceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderserviceApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(OrderserviceApplication.class, args);
+        String buildVersion = context.getEnvironment().getProperty("build.version");
+        System.out.println("Build version: " + buildVersion);
     }
 
 }
